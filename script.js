8b600f5907d8da7cd5f5a647a4d7f9afa8b52294
Nebal <nmaysaud@gmail.com>
2021-08-02 14:04:02 -0400

    add buttons and complete all requirements

--- script.js
+++ script.js
@@ -10,13 +10,54 @@ for (let i = 0; i < 256; i++) {
 
 //create a function that changes a div's color
 
-function changeColor(e) {
-    if (e.target.style.backgroundColor === 'black') {
-        e.target.style.backgroundColor = 'white';
-    } else {
-        e.target.style.backgroundColor = 'black';
+function changeColor(e) { 
+    e.target.style.backgroundColor = 'black';
+    console.log('hover');
+}
+
+// Make it so the clear grid button clears the grid
+
+clearGridButton = document.getElementById('clear-grid-button')
+clearGridButton.addEventListener('click', clearGrid);
+
+function clearGrid() {
+    squareGrid = document.querySelectorAll('.grid-square');
+
+    for (let i = 0; i < squareGrid.length; i++) {
+        squareGrid[i].style.backgroundColor = 'white';
     }
 }
 
-//add event listener for hover
+// change grid size
 
+setNewGridButton = document.getElementById('set-new-grid-button');
+setNewGridButton.addEventListener('click', setNewGrid);
+
+function setNewGrid() {
+    squareGrid = document.querySelectorAll('.grid-square');
+
+    for (let i = 0; i < squareGrid.length; i++) {
+        squareGrid[i].remove();
+    }
+
+    let newGrid = prompt('How many squares would you like in your grid? Please select a number 1-100', '16');
+
+    if (newGrid > 100 || newGrid < 0) {
+        alert('enter a number between 1 - 100');
+        prompt('How many squares would you like in your grid? Please select a number 1-100', '16');
+    } 
+
+    let newGridTotal = newGrid * newGrid;
+    
+
+    for (let i = 0; i < newGridTotal; i++) {
+        let gridSquare = document.createElement('div');
+        gridSquare.classList.add('grid-square');
+        container = document.getElementById('container');
+        container.appendChild(gridSquare);
+        gridSquare.addEventListener('mouseenter', changeColor);
+    }
+
+    container.style.gridTemplateColumns = 'repeat(' + newGrid + ', auto)';
+    container.style.gridTemplateRows = 'repeat(' + newGrid + ', auto)';
+}
